2c9089f15c7ebb27581e9df344e48f4c3af09f22
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-02-05 22:11:07 +0100

    Update intent.py

--- Loewe_scent_finder_chatbot/intent.py
+++ Loewe_scent_finder_chatbot/intent.py
@@ -2,6 +2,7 @@ import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from Loewe_scent_finder_chatbot.intent_config import INTENT_KEYWORDS, INTENTS, INTENT_PROMPT
+from Loewe_scent_finder_chatbot.config import get_gemini_llm
 
 def route_intent(text: str) -> str:
     text = text.lower()
@@ -16,12 +17,14 @@ def route_intent(text: str) -> str:
     # set up a llm here to deal with more complex intent
     if best_score >= CONFIDENCE_THRESHOLD:
       return best_intent
-
+    '''
     llm_intent_decider = ChatGoogleGenerativeAI(
         model="gemini-2.5-flash-lite",
         google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0.0,
     )
+    '''
+    llm_intent_decider = get_gemini_llm(temperature=0.0)
     chain = INTENT_PROMPT | llm_intent_decider
     llm_intent_resp = chain.invoke({"text": text}).content.strip().lower()
 
